f5596c341194a7024765df29b605cee5eb672e17
vinnysmart7777 <vinnysmart7777@gmail.com>
2022-09-05 11:38:13 +0530

    added all api's

--- menu-service/src/main/java/com/menuservice/SubscriptionServiceApplication.java
+++ menu-service/src/main/java/com/menuservice/SubscriptionServiceApplication.java
@@ -2,12 +2,18 @@ package com.menuservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+import com.Subscription.Model.Subscription;
 
 @SpringBootApplication
-public class MenuServiceApplication {
+public class SubscriptionServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MenuServiceApplication.class, args);
+		SpringApplication.run(SubscriptionServiceApplication.class, args);
 	}
+
+
+
 
 }
