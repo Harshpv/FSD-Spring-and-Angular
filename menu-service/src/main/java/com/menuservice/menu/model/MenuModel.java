5f09310c193bee13382c9f3a25899ad562eb0013
Shanmukh Reddy <mandapati.reddy@niit.com>
2022-09-08 14:26:05 +0530

    resolved pmds

--- menu-service/src/main/java/com/menuservice/menu/model/MenuModel.java
+++ menu-service/src/main/java/com/menuservice/menu/model/MenuModel.java
@@ -1,8 +1,6 @@
 package com.menuservice.menu.model;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
