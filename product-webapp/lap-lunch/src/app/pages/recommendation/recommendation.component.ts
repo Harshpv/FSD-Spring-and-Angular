dd46786942ecf830cd91dd939f371876c7ad39f1
Naufal_Mohamed <mohammednaufal2603@gmail.com>
2022-09-12 13:10:20 +0530

    Enhances Frontend

--- product-webapp/lap-lunch/src/app/pages/recommendation/recommendation.component.ts
+++ product-webapp/lap-lunch/src/app/pages/recommendation/recommendation.component.ts
@@ -7,16 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recommendation.component.css']
 })
 export class RecommendationComponent implements OnInit {
-  data: any = []
+  data: any = [];
+  city: string = "Banglore";
   constructor(private recommendation:RecommendationService) {
     this.recommendation.getData().subscribe(data=>{
-      console.log(data);
+      // console.log(data);
       this.data = data;
       
     })
    }
 
   ngOnInit(): void {
+  }
+
+  suggestByCity(city:any){
+
+    this.recommendation.getDataByCity(city).subscribe(data=>{
+      this.data = data;
+      this.city = city;
+    });
+
   }
 
 }
