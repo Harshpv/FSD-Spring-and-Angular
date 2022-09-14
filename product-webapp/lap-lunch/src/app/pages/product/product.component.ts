9fe4a68f96af5aa0d833d7a38596239a1bc26c5d
Harsha Vardhan <harshavardhanpv2000@gmail.com>
2022-09-14 10:16:44 +0530

    modified cart

--- product-webapp/lap-lunch/src/app/pages/product/product.component.ts
+++ product-webapp/lap-lunch/src/app/pages/product/product.component.ts
@@ -10,47 +10,44 @@ import { CartService } from '../menuapiservice/cart.service';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-  itemList :any[] =[];
-  searchedItems: Allitems[]=[];
-  searchKey : string="";
+  itemList: any[] = [];
+  searchedItems: Allitems[] = [];
+  searchKey: string = '';
   public filterCategory: any;
   allitems: any;
 
   @Input()
-  searchString: string="";
+  searchString: string = '';
 
-  constructor(private api: ApiserviceService, private cartService: CartService, private route: AppRoutingModule) { }
+  constructor(
+    private api: ApiserviceService,
+    private cartService: CartService,
+    private route: AppRoutingModule
+  ) {}
 
-  message:any;
- 
+  message: any;
 
   ngOnInit(): void {
     console.log(this.searchKey);
-    this.api.getItem()
-    .subscribe(res=>{
-      this.itemList =res;
-      this.filterCategory=res;
-      
-  
-      console.log(this.searchKey);
-      
-      
+    this.api.getItem().subscribe((res) => {
+      this.itemList = res;
+      this.filterCategory = res;
 
-      this.itemList.forEach((a:any)=>{
+      console.log(this.searchKey);
+
+      this.itemList.forEach((a: any) => {
         // if(a.category==="veg"){
         //   a.category="Veg Items"
-      
+
         // }
 
-        Object.assign(a,{quantity:1,total:a.itemCost});
+        Object.assign(a, { quantity: 1, total: a.itemCost });
       });
       // console.log(this.itemList);
-      
-    })
+    });
     // this.route.params.subscribe((params: { [x: string]: string; })=>{
     //   if(params['searchItem'])
 
@@ -60,94 +57,108 @@ export class ProductComponent implements OnInit {
     //    this.allitems =this.cartService.getItems();
 
     //  })
-    
-  
-    
 
-    this.api.search.subscribe((val:any)=>{
-    //  console.log(this.searchKey);
-
-      this.searchedItems=[];
-      this.searchKey=val;
-      for (let i = 0; i < this.itemList.length; i++) {
+    this.api.search.subscribe((val: any) => {
       //  console.log(this.searchKey);
-        
-        if (this.itemList[i].itemName.toLowerCase().includes(this.searchKey.toLowerCase())&& this.searchKey!="") {
+
+      this.searchedItems = [];
+      this.searchKey = val;
+      for (let i = 0; i < this.itemList.length; i++) {
+        //  console.log(this.searchKey);
+
+        if (
+          this.itemList[i].itemName
+            .toLowerCase()
+            .includes(this.searchKey.toLowerCase()) &&
+          this.searchKey != ''
+        ) {
           console.log(this.itemList[i]);
-          
+
           this.searchedItems.push(this.itemList[i]);
-          
         }
-  
       }
-    })
+    });
   }
   scroll(el: HTMLElement) {
-    el.scrollIntoView({behavior: 'smooth'});
+    el.scrollIntoView({ behavior: 'smooth' });
   }
   // addItemsToCart(item:any){
 
   //   this.cartService.addItemtocart(item);
   // }
 
-  tempdata : Cart={
-    userEmailId : "karthiga@gmail.com",
-    items: []
-    
-  }
-  tempItem: Menu = { 
-    itemId: 1,
-    itemName: "",
-    itemDescription: " ",
-    category: "",
-    itemCost:0,
-    itemImage: "",
-
-    quantity : 1,
+  tempdata: Cart = {
+    userEmailId: 'karthiga@gmail.com',
+    items: [],
   };
-  public additems(menuitem:Allitems){
+  tempItem: Menu = {
+    itemId: 1,
+    itemName: '',
+    itemDescription: ' ',
+    category: '',
+    itemCost: 0,
+    itemImage: '',
+
+    quantity: 1,
+  };
+  public additems(menuitem: Allitems) {
     // this.cart.menu.push(newitem)
-    this.tempItem.itemId = menuitem.itemId, 
-    this.tempItem.itemName =menuitem.itemName,
-    this.tempItem.itemDescription =menuitem.itemDescription,
-    this.tempItem.itemCost=menuitem.itemCost,
-    this.tempItem.itemImage=menuitem.itemImage,
-    this.tempItem.category =menuitem.category,
-    this.tempItem.quantity = 1
- 
-    this.tempdata.items.push(this.tempItem)
+    //this.tempdata = this.api.getallitems;
+    if (
+      this.tempdata.items.findIndex(
+        (item) => item.itemId === menuitem.itemId
+      ) == -1
+    ) {
+      (this.tempItem.itemId = menuitem.itemId),
+        (this.tempItem.itemName = menuitem.itemName),
+        (this.tempItem.itemDescription = menuitem.itemDescription),
+        (this.tempItem.itemCost = menuitem.itemCost),
+        (this.tempItem.itemImage = menuitem.itemImage),
+        (this.tempItem.category = menuitem.category),
+        (this.tempItem.quantity = 1);
+      this.tempdata.items.push(this.tempItem);
+    } else {
+      this.tempdata.items[
+        this.tempdata.items.findIndex((item) => item.itemId === menuitem.itemId)
+      ].quantity += 1;
+    }
 
     this.api.updateItems(this.tempdata).subscribe();
-   
- }
- 
- 
- 
+    this.tempItem = {
+      itemId: 1,
+      itemName: '',
+      itemDescription: ' ',
+      category: '',
+      itemCost: 0,
+      itemImage: '',
 
+      quantity: 1,
+    };
+  }
 
-//   addItemsToCart(newitem:any){
-// this.tempdata.menu.push(newitem)
+  //   addItemsToCart(newitem:any){
+  // this.tempdata.menu.push(newitem)
 
-//     this.api.updateItems(this.tempdata).subscribe((data)=>{
-//       console.log(data)
-//     })
-   // this.tempdata.menu.push(newitem)
+  //     this.api.updateItems(this.tempdata).subscribe((data)=>{
+  //       console.log(data)
+  //     })
+  // this.tempdata.menu.push(newitem)
 
- // }
+  // }
 
- // ngOnInit(): void {
- //     this.api.getItem().subscribe((data: Allitems[])=>{
- //       console.log(data);
- // //      this.filterCategory=data;
- //       this.items=data;
- //     });
- // }
+  // ngOnInit(): void {
+  //     this.api.getItem().subscribe((data: Allitems[])=>{
+  //       console.log(data);
+  // //      this.filterCategory=data;
+  //       this.items=data;
+  //     });
+  // }
 
- filter(category:string){
-   this.filterCategory=this.itemList
-   .filter((a:any)=>{
-     if(a.category==category||category==''){
-       return a;
-     }
-   })}
+  filter(category: string) {
+    this.filterCategory = this.itemList.filter((a: any) => {
+      if (a.category == category || category == '') {
+        return a;
+      }
+    });
+  }
 }
